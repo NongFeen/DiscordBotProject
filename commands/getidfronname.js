75c150b52e3f8972e4eff2dac3ccfa9ad0c8ac40
NongFeen <feen@DESKTOP-EC23DAK>
2023-10-21 15:59:02 +0700

    add getidfrome commands

--- commands/getidfronname.js
+++ commands/getidfronname.js
@@ -1,7 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 const axios = require('axios');
-const apiUrl = 'https://www.bungie.net/Platform/Destiny2/3/Profile/4611686018522440454/?components=100,200';
-const header = {'X-API-Key':process.env.XAPIkey_ID};
+const headers = {
+    'X-API-Key': process.env.XAPIkey_ID,
+    'Content-Type': 'application/json', //set the content type to JSON
+  };
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,23 +11,69 @@ module.exports = {
 		.setDescription('getidfronname')
 		.addStringOption(option =>
 			option.setName('input')),
-	async execute(interaction) {
-		var bungieGlobalDisplayName;
-		var bungieGlobalDisplayNameCode;
-		// await axios.get(apiUrl, { header })
-		await await axios.get(apiUrl, {header})
-		.then((response) => {
-			console.log(response.data);
-			bungieGlobalDisplayName = response.data.Response.profile.data.userInfo.bungieGlobalDisplayName;	
-			bungieGlobalDisplayNameCode = response.data.Response.profile.data.userInfo.bungieGlobalDisplayNameCode;	
-			console.log("Bungie Global Display Name:", bungieGlobalDisplayName+"#"+bungieGlobalDisplayNameCode);
-			interaction.reply('Bungie Global Display Name : '+ bungieGlobalDisplayName+'#'+bungieGlobalDisplayNameCode);
-		})
-		.catch((error) => {
-			// Handle any errors that occurred during the request
-			console.error(error);
-		});
-		// const text = interaction.options.get('input');
-		// console.log(text.value);
-	},
+async execute(interaction) {
+  const inputString = interaction.options.get('input').value;
+  const part = inputString.split('#');
+
+  const bungieGlobalDisplayName = part[0]; // string
+  const bungieGlobalDisplayNameCode = part[1]; // #xxxx int
+
+  const apiUrl = 'https://www.bungie.net/Platform/User/Search/GlobalName/';
+  
+
+  const resultsPerPage = 25;
+  let page = 0;
+  let dataCount =0;
+  let membershipId;
+  let found =false;
+  console.log(bungieGlobalDisplayName + "  " + bungieGlobalDisplayNameCode);
+  console.log(`${apiUrl}${page}`);
+
+  try {
+    while (!found) {
+      const response = await axios.post(`${apiUrl}${page}`, { "displayNamePrefix": bungieGlobalDisplayName }, { headers });
+      console.log(response.data.Response);
+      const responseData = response.data.Response;
+      for (const result of responseData.searchResults) {
+        dataCount++;
+        console.log(dataCount + " : " + `${apiUrl}${page}`)
+        // console.log(result.bungieGlobalDisplayNameCode+" : "+ parseInt(bungieGlobalDisplayNameCode) + (result.bungieGlobalDisplayName === bungieGlobalDisplayName).toString());
+        if (result.bungieGlobalDisplayName === bungieGlobalDisplayName &&
+            result.bungieGlobalDisplayNameCode === parseInt(bungieGlobalDisplayNameCode)
+        ){
+          console.log("found")
+          found=true;
+          for (const membership of result.destinyMemberships) {
+            if(membership.bungieGlobalDisplayNameCode==bungieGlobalDisplayNameCode){
+              membershipId = membership.membershipId;
+              break;
+            }
+          }
+        }
+      }
+      if (responseData.hasMore&&dataCount>=24 &&!found) {//24 work due to Max data count per page
+        // If there are more pages, increment the page number
+        console.log("page = " + page + " url = " + `${apiUrl}${page}`)
+        page++;
+        dataCount=0;
+      } else {
+        // No more pages to retrieve
+        break;
+      }
+    }
+    if (membershipId) {
+      // Reply back to the interaction with the membershipId
+      let str = `Membership ID for ${bungieGlobalDisplayName}#${bungieGlobalDisplayNameCode} is ${membershipId}`;
+      await interaction.reply(str);
+    } else {
+    //   // Reply back if no matching entry is found
+      await interaction.reply(`No matching entry found for ${bungieGlobalDisplayName}#${bungieGlobalDisplayNameCode}`);
+    }
+    
+  } catch (error) {
+    // console.error('Error:', error);
+    // Handle and reply with an error message
+    await interaction.reply('An error occurred while fetching the data from Bungie API.');
+  }
+}
 };
